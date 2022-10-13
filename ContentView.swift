a77003bae44cff5b7576730d87052db25a2f9e3a
Haden Miller <s0541780@student.rockvalleycollege.edu>
2022-10-13 09:21:06 -0500

    touch controls

--- ContentView.swift
+++ ContentView.swift
@@ -2,18 +2,26 @@ import SwiftUI
 import SpriteKit
 
 class GameScene:SKScene {
+    let texture1 = SKTexture(imageNamed: "snail_00");
+    let texture2 = SKTexture(imageNamed: "snail_04");
+    let texture3 = SKTexture(imageNamed: "snail_07");
+    let character:SKSpriteNode = SKSpriteNode(imageNamed: "snail_00");
+    
     override func sceneDidLoad() {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame);
-        let texture1 = SKTexture(imageNamed: "snail_00");
-        let texture2 = SKTexture(imageNamed: "snail_04");
-        let texture3 = SKTexture(imageNamed: "snail_07");
         let textures:Array<SKTexture> = [texture1, texture2, texture3];
-        let character:SKSpriteNode = SKSpriteNode(imageNamed: "snail_00");
         character.texture = texture1;
         character.position = CGPoint(x:200, y:200);
         character.size = CGSize(width:100, height:80);
         addChild(character);
         character.run(SKAction.repeatForever(SKAction.animate(with: textures, timePerFrame: 0.2)));
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let location = touch.location(in: self);
+        let position = CGPoint(x: location.x, y: 200);
+        character.run(SKAction.move(to: position, duration: 3));
     }
 }
 
